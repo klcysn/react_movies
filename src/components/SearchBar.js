e50d4b4779703f4fb2fea53ed587b36edca64a38
klcysn <klcysn@users.noreply.github.com>
2021-01-09 23:32:08 +0100

    Add search function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,21 +1,19 @@
 
 
 const SearchBar = (props) =>{
+  
     return (
       <div className="mb-4">
         <nav class="navbar navbar-light bg-light w-100">
           <div class="container-fluid w-100">
-            <form class="d-flex w-100">
+            <form class="d-flex col-12">
               <input
-                className="form-control col-10"
+                className="form-control col-12"
                 type="search"
-                placeholder="Search"
+                placeholder="Search Movie..."
                 aria-label="Search"
                 onChange={props.handleSearch}
               />
-              <button className="btn btn-outline-success col-2" type="submit">
-                Search
-              </button>
             </form>
           </div>
         </nav>
